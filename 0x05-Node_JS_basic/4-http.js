a83dd51a5ee56b62a461f96c47c3d6ee93f476f6
bosirex <dennis.dancan@gmail.com>
2024-04-03 09:34:18 +0300

    Update 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,21 +1,8 @@
 const http = require('http');
 
-const PORT = 1245;
-const HOST = 'localhost';
-const app = http.createServer();
-
-
-app.on('request', (_, res) => {
-  const responseText = 'Hello Holberton School!';
-
-  res.setHeader('Content-Type', 'text/plain');
-  res.setHeader('Content-Length', responseText.length);
-  res.statusCode = 200;
-  res.write(Buffer.from(responseText));
-});
-
-app.listen(PORT, HOST, () => {
-  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
-});
+const app = http.createServer((req, res) => {
+  res.write('Hello Holberton School!');
+  res.end();
+}).listen(1245);
 
 module.exports = app;
